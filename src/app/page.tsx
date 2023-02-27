cd8ad07dd0150c5e0f9c311d3806c0959aaa1b59
engageintellect <jc9361@gmail.com>
2023-02-26 21:32:56 -0800

    init grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,20 +20,25 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute top-0 h-full w-full">
+      <div className="absolute top-0 h-full w-full" id="main">
         <div className="flex items-center justify-center h-full">
-          <div className="flex flex-col px-6">
-            <div className="text-white font-semibold text-8xl drop-shadow-md">
-              Hello World
+          <a href="/#grid">
+            <div className="flex flex-col px-6">
+              <div className="text-white font-semibold text-8xl drop-shadow-md">
+                Hello World
+              </div>
+              <div className="text-zinc-300 text-2xl drop-shadow-md">
+                Hello World
+              </div>
             </div>
-            <div className="text-zinc-300 text-2xl drop-shadow-md">
-              Hello World
-            </div>
-          </div>
+          </a>
         </div>
       </div>
 
-      <div className="min-h-screen max-w-7xl mx-auto flex items-center">
+      <div
+        className="min-h-screen max-w-7xl mx-auto flex items-center"
+        id="grid"
+      >
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-5">
           <div className="bg-zinc-800 text-zinc-300 p-5 rounded-lg">
             <div className="text-[24px] font-semibold">Hello World</div>
